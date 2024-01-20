512d680c4a12abae279279db5d447f7ec83605f1
auriceliof <auricelio.suporte@hotmail.com>
2024-01-20 12:49:22 -0300

    04-09 Requisições para o catálogo e por id

--- dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -2,11 +2,24 @@ import './styles.css';
 import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
-import * as productService from '../../../services/product-service';
+import { useEffect, useState } from 'react';
+import { ProductDTO } from '../../../models/product';
+import axios from 'axios';
 
 export default function Catalog() {
-  return (
 
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+  useEffect(() => {
+
+    axios.get("http://localhost:8080/products?size=12")
+      .then(response => {
+        setProducts(response.data.content);
+      });
+
+  }, []);
+
+  return (
     <main>
       <section id="catalog-section" className="dsc-container">
 
@@ -14,7 +27,7 @@ export default function Catalog() {
 
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {
-            productService.findAll().map(
+            products.map(
               product => <CatalogCard key={product.id} product={product} />
             )
           }          
@@ -24,6 +37,5 @@ export default function Catalog() {
 
       </section>
     </main>
-
   );
 }
--- dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,18 +2,20 @@ import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
 import axios from "axios";
 
 export default function ProductDetails() {
 
+  const params = useParams();
+
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
 
-    axios.get("http://localhost:8080/products/2")
+    axios.get(`http://localhost:8080/products/${params.productId}`)
       .then(response => {console.log(response.data);
         setProduct(response.data);
       });
